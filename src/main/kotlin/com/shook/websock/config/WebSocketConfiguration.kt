9666d4798526a7c808bc3e3f55e44dfab2926d56
Hamp <yongbeomkwak@gmail.com>
2024-11-23 19:50:42 +0900

    :recycle: :: 이름 변경

--- src/main/kotlin/com/shook/websock/config/WebSocketConfiguration.kt
+++ src/main/kotlin/com/shook/websock/config/WebSocketConfiguration.kt
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
-import com.shook.websock.handler.WebsocketHandler
+import com.shook.websock.handler.WebSocketHandler
 
 @Configuration
 @EnableWebSocket
 class WebSocketConfiguration: WebSocketConfigurer {
-    private val webSocketHandler: WebsocketHandler = WebsocketHandler()
+    private val webSocketHandler: WebSocketHandler = WebSocketHandler()
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(webSocketHandler,"/ws/chat")
--- src/main/kotlin/com/shook/websock/handler/WebSocketHandler.kt
+++ src/main/kotlin/com/shook/websock/handler/WebSocketHandler.kt
@@ -6,7 +6,7 @@ import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 
 @Component
-public class WebsocketHandler :  TextWebSocketHandler() {
+public class WebSocketHandler :  TextWebSocketHandler() {
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         val payload = message.payload
         val textMessage = TextMessage("Welcome chatting server")
